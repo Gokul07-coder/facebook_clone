ad1dbda482fb93b018e5db959b5a21e96cffb141
Gokul <81502324+Gokul07-coder@users.noreply.github.com>
2023-03-17 11:00:23 +0530

    second commit

--- App.js
+++ App.js
@@ -7,16 +7,20 @@ const fb_post = require('./routes/post_routes/post');
 const edit_profile = require('./routes/edit_profile/edit_profile');
 const settings = require('./routes/edit_profile/edit_general');
 const friend = require('./routes/friend_req/friend_req');
+const group = require('./routes/group/group');
+const home = require('./routes/homepage.js/homepage')
+const message = require('./routes/message/message')
 const e = require('express');
-// const {PORT} = process.env;
 
 app.use(index)
 app.use(fb_post);
 app.use(edit_profile);
 app.use(settings);
 app.use(friend);
+app.use(group);
+app.use(home);
+app.use(message);
 
 app.listen(process.env.PORT, ()=>{
     console.log("Listening to port : 8000");
 });
-// app.listen(PORT)
